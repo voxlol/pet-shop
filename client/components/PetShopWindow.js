d3b55f6ef6f18b622c3146561866d6426d5db600
MKS-students <mks.students@hackreactor.com>
2015-06-26 18:57:28 -0500

    Refactored signin/signout buttons

--- client/components/PetShopWindow.js
+++ client/components/PetShopWindow.js
@@ -9,8 +9,7 @@ PetShopWindow.controller = function () {
 
   ctrl.username = m.prop("");
   ctrl.password = m.prop("");
-
-  ctrl.isSignedIn = m.prop('false');
+  ctrl.isSignedIn = m.prop(false);
 
   ctrl.shop = m.prop(null)
   ctrl.pets = m.prop(null)
@@ -24,6 +23,7 @@ PetShopWindow.controller = function () {
       // an error exists
     }else{
       // Probably want to sign in immediately right after signing up
+      Shop.signIn(ctrl.username(), ctrl.password());
     }
 
   }
@@ -37,15 +37,33 @@ PetShopWindow.controller = function () {
       // grab returnData's id, username under the user property
       // also can grab the token under the returnData.apiToken property
       // set the ctrl.isSignedIn property to true
+      ctrl.isSignedIn = m.prop(true);
     }
+  }
 
+  ctrl.signOut = function(e){
+    e.preventDefault();
+    ctrl.isSignedIn(false);
   }
 }
 
 PetShopWindow.view = function (ctrl) {
 
+  // Base case : 2 sign in / sign up
+  if (ctrl.isSignedIn() === false){
+    var signInButton = m('button', {type:'submit', onclick: ctrl.signIn}, "Sign In");
+    var signUpButton = m('button', {type:'submit', onclick: ctrl.signUp}, "Sign Up");
+  }else{
+    var signInButton = null;
+    var signUpButton = null;
+    //functionality pending
+    var signOutButton = m('button', {type:'submit', onclick: ctrl.signOut}, "Sign Out");
+  }
+
   return m('.pet-shop', [
     m('h1', "Welcome to " + ctrl.shop().name),
+
+
 
     //FORM
     m('form', [
@@ -65,29 +83,32 @@ PetShopWindow.view = function (ctrl) {
       }),
 
       //SUBMIT BUTTON
-      m('button', {type: "submit", onclick: ctrl.signUp}, "Submit")
+      // m('button', {type: "submit", onclick: ctrl.signUp}, "Submit")
+      signInButton,
+      signUpButton,
+      signOutButton
     ]),
 
-        //FORM - ---------------- This code needs to be refactored
-    m('form', [
+    //     //FORM - ---------------- This code needs to be refactored
+    // m('form', [
 
-      "User Name:  ",
-      m('input', {
-        type :"text",
-        name: "username",
-        oninput: m.withAttr("value", ctrl.username)
-      }),
+    //   "User Name:  ",
+    //   m('input', {
+    //     type :"text",
+    //     name: "username",
+    //     oninput: m.withAttr("value", ctrl.username)
+    //   }),
 
-      " Password:  ",
-      m('input', {
-        type: "text",
-        name: "password",
-        oninput: m.withAttr("value", ctrl.password)
-      }),
+    //   " Password:  ",
+    //   m('input', {
+    //     type: "text",
+    //     name: "password",
+    //     oninput: m.withAttr("value", ctrl.password)
+    //   }),
 
-      //SUBMIT BUTTON
-      m('button', {type: "submit", onclick: ctrl.signIn}, "Submit")
-    ]),
+    //   //SUBMIT BUTTON
+    //   m('button', {type: "submit", onclick: ctrl.signIn}, "Submit")
+    // ]),
 
     //partial application so petView has access to controller
     ctrl.pets().map( petView.bind(null, ctrl) )
@@ -95,7 +116,9 @@ PetShopWindow.view = function (ctrl) {
 }
 
 function petView (ctrl, pet){
-  // Make a button pop up if there's a signed in user
+  // check the isSignedIn property
+  // var likedButton = isSignedIn ? likedButton element : ""
+
   return m('.pet', [
     m('p', 'Name : ' + pet.name),
     m('p', 'Species : ' + pet.species),
